20712f23e66c9cb27dfb76d038617f2a7a843abc
Radoslaw <tidecode.radoslawh@gmail.com>
2024-03-05 09:23:09 +0100

    fix import

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 from playwright.sync_api import sync_playwright
 import os
-from .models.tweeter import TweetDetails
-from .db.mongo import get_database, save_to_db
-from .scrappers.reddit import RedditScrapper
-from .enums.xpaths import TwitterXPathEnum
+from models.tweeter import TweetDetails
+from db.mongo import get_database, save_to_db
+from scrappers.reddit import RedditScrapper
+from enums.xpaths import TwitterXPathEnum
 
 
 os.environ.get('twitter_mail')
